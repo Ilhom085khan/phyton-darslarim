9a85d0de9ba52331d02e373fe5fee3da5dacc3f9
Ilhom085khan <124565171+Ilhom085khan@users.noreply.github.com>
2023-02-06 14:38:58 +0500

    yangi fayllar qowildi
    3- va 4- darslar yuklandi

---
+++ 3-dars.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  6 14:31:33 2023
+
+@author: VICTUS
+"""
+
+#"Nexia", "Tico", 'Damas' ko'rganlar qilar havas
+print("\"Nexia\",\"Tico\",'Damas' korganlar qilar havas")
+
+#5 ning 4-darajasini toping
+print(5**4)
+
+#22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+print(22%4)
+
+#Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+print("Tomonlari 125ga teng kvadratning yuzi ",125**2," va perimetri",125*4," ga teng!")
+
+#Diametri 12 ga teng bo'lgan doiraning yuzini toping (π=3.14 deb oling)
+pi=3.14
+r=6
+print("Diametri 12 ga teng bolgan doiraning yuzi ",r**2*pi)
---
+++ 4-dars.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  6 14:33:49 2023
+
+@author: VICTUS
+"""
+
+#Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping (Pifagor teoremasidan foydalaning)
+print("gipatenuza",(6**2+7**2)**(1/2))
+
+#"Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring
+i='Helo World'
+
+
+#xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring, keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
+
+
+#class den nomlangan o'zgaruvchi yarating, unga biror qiymat bering va konsolga chiqaring (siz kutgan natija chiqdimi?) Quyidagi kodni bajaring:
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
